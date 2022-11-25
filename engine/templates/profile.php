5d54aa1aca4956b51b6b263696620eda82ed041d
Kirill Bokov <118357712+Kirill-Bokov@users.noreply.github.com>
2022-11-25 23:07:37 +0300

    Более-менее закончена работа с профилями

--- engine/templates/profile.php
+++ engine/templates/profile.php
@@ -1,21 +1,39 @@
+<?php
+require_once "./engine/models/adver.php";
+
+$db = open_database_connection();
+$data = getUserData($uid);
+$format = "";
+$age = 0;
+$myaddr = "";
+$advers = array();
+if($data != null)
+{
+    $format = getPhotoFormat($data['avatar'], false);
+    if($format == "")
+        $data['avatar'] = 'default.jpg';
+    $age = getAge($data['birthday']);
+    $myaddr = getAddr($uid);
+    $advers = getAdversOfUser($uid);
+    $houses = getHousesOfUser($uid);
+    $addr = getHousesAddress($houses);
+}
+?>
 <html><head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="./engine/js/main.js"></script>
     <link rel="stylesheet" href="./main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="./engine/js/main.js"></script>
+    
+    
+        <title>Мой профиль</title>
         <link rel="stylesheet" href="./main.css">
-        
-        
-            <title>Test1 Test2    </title>
-            <link rel="stylesheet" href="./main.css">
-            <link rel="stylesheet" href="./engine/styles/header_main_page.css">
-            <link rel="stylesheet" href="./engine/styles/profile.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script type="text/javascript" src="./engine/js/Sliding_Panel.js"></script>
-            <script type="text/javascript" src="./engine/js/advers.js"></script>
-            <script type="text/javascript" src="./engine/js/main.js"></script>
-            <meta charset="utf-8">
-        <style type="text/css">@font-face { font-family: Roboto; src: url("chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/css/Roboto-Regular.ttf"); }</style><style type="text/css">@font-face { font-family: Roboto; src: url("chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/css/Roboto-Regular.ttf"); }</style>
+        <link rel="stylesheet" href="./engine/styles/header_main_page.css">
+        <link rel="stylesheet" href="./engine/styles/profile.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript" src="./engine/js/Sliding_Panel.js"></script>
+        <script type="text/javascript" src="./engine/js/advers.js"></script>
+        <script type="text/javascript" src="./engine/js/main.js"></script>
+        <meta charset="utf-8">
+    <style type="text/css">@font-face { font-family: Roboto; src: url("chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/css/Roboto-Regular.ttf"); }</style>
     <style>
     /* width */
     ::-webkit-scrollbar {
@@ -35,91 +53,58 @@
         border-radius: 10px;
         background-color: #D9D9D9;
     }
-    
-    
-    </style>
-    </head>
-        <body>
-            <header> <div class="header-panel">
+    </style></head>
+    <body> <header> <div class="header-panel">
   	<div class="container" onclick="Sliding_Panel()">
   		<img src="../../engine/images/munu_bar.svg" id="menu_bar_image">
   		<div id="Sliding_Panel" class="Sliding_Panel">
   			<button class="To_Add_Adver_Button" onclick="navigateTo('./addadver')">Создать объявление</button>
   			<button class="To_Add_Adver_Button" onclick="navigateTo('./tags')">Тэги</button>
   		</div>
-  	</div>
-  	<img src="../../engine/images/domteam_logo.svg" id="main_page_logo">
+        </div>
+        <img src="../../engine/images/domteam_logo.svg" id="main_page_logo">
+      </div>
+  
+  </header>
+  
+  <div class="background_site">
+    <div id="block_user_info" class="block_user_info"></div>
+    <?php if($data != null) { ?>
+      <img id="user_photo" class="user_photo" src="./engine/storage/<?php echo $data['avatar'].$format; ?>">
+  
+  <div id="user_name" class="user_name"><?php echo $data['name']." ".$data['surname']; ?></div>
+  <div id="person_info" class="person_info">Информация о человеке:</div>
+  <div id="user_age" class="user_age">Возраст: <?php echo $age; ?></div>
+  <div id="user_address" class="user_address">Адрес: <?php echo $myaddr; ?></div>
+  <div id="phone_number" class="phone_number">Номер телефона: <?php echo $data['phone']; ?></div><div id="about_me" class="about_me">Обо мне:
+  
+  <?php echo $data['about_me']; ?></div><div id="block_advers" class="block_advers">
+                    <?php for($i = 0; $i < count($advers); $i++) {
+            $tagsid = json_decode($advers[$i]['tags']);
+            $tags = getAdverTags($tagsid);
+        ?>
+        <div class="topic"><?php echo $advers[$i]['topic']; ?></div><br><br>
+        <?php if($advers[$i]['photo_id'] != "") { ?>
+        <div><img class="picture" src="./engine/storage/<?php echo $advers[$i]['photo_id'].getPhotoFormat($advers[$i]['photo_id']); ?>"></div>
+    <?php } ?>
+        <div class="time"><?php echo date('Y-m-d H:i', $advers[$i]['time']); ?></div><br>
+        <?php if(count($tagsid) > 0) { ?>
+        <div class="tags">Теги: <?php echo implode(',', $tags); ?></div><br>
+    <?php } ?>
+        <div class="hid">Адрес дома: <?php echo $addr[array_search($advers[$i]['hid'], $houses)]; ?></div><br>
+        <div class="text"><?php echo $advers[$i]['text']; ?></div><hr>
+    <?php }
+    close_database_connection($db);
+     ?>
+                  </div>
+                  <?php } else { ?>
+                    <div class="person_info">Профиль не существует!</div>
+                  <?php } ?>
+  </div>
+  
+  <footer>
     
-  	  	</div>
-                    
-        
-                
-      </header>
-      
-      <div class="background_site">
-              <div id="block_user_info" class="block_user_info"></div>
-    <img id="user_photo" class="user_photo" src="./engine/storage/default.jpg">
-      
-      <div id="user_name" class="user_name">Test1 Test2</div>
-      <div id="person_info" class="person_info">Информация о человеке:</div>
-      <div id="user_age" class="user_age">Возраст: 2022</div>
-      <div id="user_address" class="user_address">Адрес: </div>
-      <div id="phone_number" class="phone_number">Номер телефона: </div><div id="about_me" class="about_me">Обо мне:
-      
-      </div><div id="block_advers" class="block_advers">
-        <div id="scroll">
-          <div class="overflow"></div>
-    </div></div>
-    
-                        </div>
-      
-      <footer>
-        
-      </footer>
-      
-      
-      <script>
-            var returnedSuggestion = ''
-            let editor, doc, cursor, line, pos
-            document.addEventListener("keydown", (event) => {
-            setTimeout(()=>{
-                editor = event.target.closest('.CodeMirror');
-                if (editor){
-                    doc = editor.CodeMirror.getDoc()
-                    cursor = doc.getCursor()
-                    line = doc.getLine(cursor.line)
-                    pos = {line: cursor.line, ch: line.length}
-                    if (event.key == "Enter"){
-                        var query = doc.getRange({ line: Math.max(0,cursor.line-10), ch: 0 }, { line: cursor.line, ch: 0 })
-                        window.postMessage({source: 'getSuggestion', payload: { data: query } } )
-                        //displayGrey(query)
-                    }
-                    else if (event.key == "ArrowRight"){
-                        acceptTab(returnedSuggestion)
-                    }
-                }
-            }, 0)
-            })
-    
-            function acceptTab(text){
-            if (suggestionDisplayed){
-                doc.replaceRange(text, pos)
-                suggestionDisplayed = false
-            }
-            }
-            function displayGrey(text){
-            var element = document.createElement('span')
-            element.innerText = text
-            element.style = 'color:grey'
-            var lineIndex = pos.line;
-            editor.getElementsByClassName('CodeMirror-line')[lineIndex].appendChild(element)
-            suggestionDisplayed = true
-            }
-            window.addEventListener('message', (event)=>{
-            if (event.source !== window ) return
-            if (event.data.source == 'return'){
-                returnedSuggestion = event.data.payload.data
-                displayGrey(event.data.payload.data)
-            }
-            })
-        </script></body></html>
+  </footer>
+  
+  
+  </body></html>
